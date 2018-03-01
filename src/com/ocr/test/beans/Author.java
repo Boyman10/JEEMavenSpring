89e903c03e7bd8279bd6df0a67d8f33de9ad209e
Eddy Mio <contact@remotehireman.fr>
2018-03-01 09:29:47 +0100

    Transforming a JEE project into a Maven one implementing Spring MVC

---
+++ src/com/ocr/test/beans/Author.java
@@ -0,0 +1,29 @@
+package com.ocr.test.beans;
+
+public class Author {
+
+	private String name;
+	private String firstnam;
+	private boolean active;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstnam() {
+		return firstnam;
+	}
+	public void setFirstnam(String firstnam) {
+		this.firstnam = firstnam;
+	}
+	public boolean isActive() {
+		return active;
+	}
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
+}
